fa21e99b8c54f0afea7d15270717e3ef10ef9fb4
Solkaz <jheld135@gmail.com>
2016-10-19 22:53:07 -0500

    Added ability to add a new alias for list, rename and search commands if the alias doesn't already exist

--- conf.py
+++ conf.py
@@ -21,10 +21,22 @@ class Conf():
     def DoesAliasExist(self, alias_phrase):
         return alias_phrase in self.aliases
 
+    def HandleDoesNotExist(self, alias_phrase):
+        utils.print_does_not_exist(alias_phrase)
+        self.OfferToAdd(alias_phrase)
+
+    def OfferToAdd(self, alias):
+        alias_file_path = utils.offer_to_add(alias)
+
+        # User chose to add the alias and entered a file_path
+        if alias_file_path is not None:
+            self.AddAlias(alias, alias_file_path)
+
     def AddAlias(self, alias_to_add, file_path):
         # Check that the alias pre-exists
         if self.DoesAliasExist(alias_to_add):
-            print('alias ' + alias_to_add + " already exists at " + file_path)
+            print('alias ' + alias_to_add + " already exists at "
+                  + self.aliases[alias_to_add])
             # TODO: offer to remap file or to specify a new alias
         else:
             self.aliases[alias_to_add] = file_path
@@ -40,8 +52,7 @@ class Conf():
         elif self.DoesAliasExist(alias_phrase):
             utils.print_alias(alias_phrase, self.aliases[alias_phrase])
         else:
-            utils.print_does_not_exist(alias_phrase)
-            # TODO: offer to add alias
+            self.HandleDoesNotExist(alias_phrase)
 
     def Search(self, search_phrase):
         matched_keys = []
@@ -54,12 +65,12 @@ class Conf():
                 utils.print_alias(key, self.aliases[key])
 
         else:
-            print('no aliases matched')
+            print('no aliases matched for ' + search_phrase)
+            self.OfferToAdd(search_phrase)
 
     def Rename(self, old_alias, new_alias):
         if not self.DoesAliasExist(old_alias):
-            utils.print_does_not_exist(old_alias)
-            # TODO: offer to add alias
+            self.HandleDoesNotExist(old_alias)
         else:
             if not old_alias == new_alias:
                 self.aliases[new_alias] = self.aliases.pop(old_alias)
--- utils.py
+++ utils.py
@@ -9,6 +9,16 @@ def print_does_not_exist(alias):
     print('Alias ' + alias + ' does not exist')
 
 
+def offer_to_add(alias):
+    resp = input('Do you want to add ' + alias + '? [y/n]: ')
+    while (resp not in ['y', 'n']):
+        resp = input("Please answer [y/n]: ")
+    if resp == 'y':
+        file_path = input("Enter the path to the file that " + alias
+                          + ' points to: ')
+        return file_path
+
+
 def make_sorted_dict(unordered):
     # Create an OrderedDict from a regular dict
     # It will be sorted by the key's values
